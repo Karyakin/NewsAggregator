00438ed8464435784a2bb759ffcca24561d2f436
Дмитрий Карякин <375447955552dk@gmail.com>
2021-03-22 17:55:10 +0300

    22.03.2021

---
+++ RepositoriesInterfaces/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoriesInterfaces
+{
+    public interface IRepositoryBase<T>
+    {
+        Task GetById(T entity);
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task<IQueryable<T>> FindAll(bool trackChanges);
+        Task<IQueryable<T>> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+    }
+}

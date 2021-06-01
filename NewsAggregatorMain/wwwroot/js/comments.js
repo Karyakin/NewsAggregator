f89773a48e523ff4849b1d7def69daa1908b3641
Дмитрий Карякин <375447955552dk@gmail.com>
2021-06-01 17:54:06 +0300

    01.06/2021(18:00)

---
+++ NewsAggregatorMain/wwwroot/js/comments.js
@@ -0,0 +1,40 @@
+﻿
+
+let commentDispleySwitherElement = document.getElementById('comments-display-switcher');
+
+commentDispleySwitherElement.onmouseover = function () {
+    commentDispleySwitherElement.className = commentDispleySwitherElement.className.replace('btn-primary', 'btn-info');
+}
+
+commentDispleySwitherElement.onmouseout = function () {
+    commentDispleySwitherElement.className = commentDispleySwitherElement.className.replace('btn-info', 'btn-primary');
+}
+
+commentDispleySwitherElement.onmousedown = function () {
+    commentDispleySwitherElement.className = commentDispleySwitherElement.className.replace('btn-info', 'btn-secondary');
+}
+
+commentDispleySwitherElement.onmouseup = function () {
+    commentDispleySwitherElement.className = commentDispleySwitherElement.className.replace('btn-secondary','btn-info' );
+}
+
+let isCommentsDispley = false;
+
+function loadComments() {
+    if (isCommentsDispley==true) {
+        commentDispleySwitherElement.innerHTML = 'Display comments';
+        document.getElementById('comments-container').innerHTML='';
+    }
+    else {
+        commentDispleySwitherElement.innerHTML = 'Hide comments';
+        let comments = document.getElementById('comments-container');
+        for (var i = 0; i < 10; i++) {
+
+        }
+
+        let aa = document.contains. innerHTML.comments;
+        let aaa= document.innerHTML.comments;
+
+    }
+    isCommentsDispley = !isCommentsDispley;
+}
